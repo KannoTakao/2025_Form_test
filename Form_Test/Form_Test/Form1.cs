29121cf4fd943dfab17d1485ceb8c4e8ec4699b4
rusty <rusty0723@icloud.com>
2025-11-02 15:47:38 +0900

    aaaaaaa

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -11,10 +11,56 @@ using System.Windows.Forms;
 namespace Form_Test
 {
     public partial class Form1 : Form
-    {
+    { // constをつけると初期化時にのみ値の変更が可能になる
+
+        /// <summary>
+        /// ボタンの横幅
+        /// </summary>
+        const int BUTTON_SIZE_X = 100;
+        /// <summary>
+        /// ボタンの縦
+        /// </summary>
+        const int BUTTON_SIZE_Y = 100;
+
+
+        /// <summary>
+        /// ボタンが横に何個並ぶか
+        /// </summary>
+        const int BOARD_SIZE_X = 3;
+        /// <summary>
+        /// ボタンが縦に何個並ぶか
+        /// </summary>
+        const int BOARD_SIZE_Y = 3;
+
+        private TestButton[,] _buttonArray;
+
         public Form1()
         {
             InitializeComponent();
+            //_buttonArrayの初期化
+            _buttonArray = new TestButton[BOARD_SIZE_Y, BOARD_SIZE_X];
+
+            for (int i = 0; i < BOARD_SIZE_X; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE_Y; j++)
+                {
+                    // インスタンスの生成
+                    TestButton testButton = new TestButton(new Point(BUTTON_SIZE_X * i, BUTTON_SIZE_Y * j), new Size(BUTTON_SIZE_X, BUTTON_SIZE_Y), "");
+
+                    // 配列にボタンの参照を追加
+                    _buttonArray[j, i] = testButton;
+
+                    // コントロールにボタンを追加
+                    Controls.Add(testButton);
+                }
+            }
+
+            _buttonArray[1, 0].SetEnable(true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Form_Test/Form_Test/TestButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Form_Test
+{
+    internal class TestButton : Button
+    {
+        /// <summary>onの時の色</summary>
+        private Color _onColor = Color.LightBlue;
+
+        /// <summary>offの時の色</summary>
+        private Color _offColor = Color.DarkBlue;
+
+        /// <summary>現在OnかOffか</summary>
+        private bool _enable;
+
+
+        /// <summary>onとoffの設定</summary> 
+        /// <param name="on"></param>
+        public void SetEnable(bool on)
+        {
+            _enable = on;
+            if (on)
+            {
+                BackColor = _onColor;
+            }
+            else
+            {
+                BackColor = _offColor;
+            }
+        }
+        public TestButton(Point position, Size size, string text)
+        {
+            // ボタンの位置を設定
+            Location = position;
+            // ボタンの大きさを設定
+            Size = size;
+            // ボタン内のテキストを設定
+            Text = text;
+
+            SetEnable(false);
+
+            Click += ClickEvent;
+        }
+        private void ClickEvent(object sender, EventArgs e)
+        {
+            SetEnable(!_enable);
+        }
+    }
+}
+
