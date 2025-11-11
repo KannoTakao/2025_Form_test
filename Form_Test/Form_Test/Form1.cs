cbca79655e1e10f2086512f27459c77b6d2df83b
rusty <rusty0723@icloud.com>
2025-11-11 10:14:56 +0900

    最終コミット(多分)

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -1,63 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Form_Test
 {
     public partial class Form1 : Form
-    { // constをつけると初期化時にのみ値の変更が可能になる
-
-      
+    {
         const int BUTTON_SIZE_X = 100;
-  
         const int BUTTON_SIZE_Y = 100;
- 
         const int BOARD_SIZE_X = 3;
-   
         const int BOARD_SIZE_Y = 3;
 
         private TestButton[,] _buttonArray;
-
+        private Random _random = new Random();
 
         public Form1()
         {
             InitializeComponent();
-
-            //_buttonArrayの初期化
             _buttonArray = new TestButton[BOARD_SIZE_Y, BOARD_SIZE_X];
 
             for (int i = 0; i < BOARD_SIZE_X; i++)
             {
                 for (int j = 0; j < BOARD_SIZE_Y; j++)
                 {
-                    // インスタンスの生成
-                    TestButton testButton = new TestButton(this,i,j,
-                        new Size(BUTTON_SIZE_X, BUTTON_SIZE_Y),
-                        "");
-
-                    // 配列にボタンの参照を追加
+                    TestButton testButton = new TestButton(this, i, j, new Size(BUTTON_SIZE_X, BUTTON_SIZE_Y), "");
                     _buttonArray[j, i] = testButton;
-
-                    // コントロールにボタンを追加
                     Controls.Add(testButton);
                 }
             }
-            GetTestButton(0,0).SetEnable(false);
+
+            InitializeBoard();
         }
-  
-        public TestButton GetTestButton(int x, int y) 
+
+        public TestButton GetTestButton(int x, int y)
         {
-            //配列外参照
             if (x < 0 || x >= BOARD_SIZE_X) return null;
             if (y < 0 || y >= BOARD_SIZE_Y) return null;
-
             return _buttonArray[y, x];
+        }
+
+        public void InitializeBoard()
+        {
+            for (int i = 0; i < BOARD_SIZE_X; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE_Y; j++)
+                {
+                    bool randomButton = _random.Next(2) == 0;
+                    GetTestButton(i, j).SetEnable(randomButton);
+                }
+            }
+        }
+
+        public void CheckClear()
+        {
+            bool message = _buttonArray[0, 0].IsEnabled();
+            foreach(var DiggyMo in _buttonArray)
+            {
+                if ( DiggyMo.IsEnabled() != message)
+                    return;
+            }
+
+            MessageBox.Show("クリアしました！");
+            InitializeBoard();
         }
 
         private void Form1_Load(object sender, EventArgs e)
--- Form_Test/Form_Test/TestButton.cs
+++ Form_Test/Form_Test/TestButton.cs
@@ -1,81 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Text;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Windows.Forms.LinkLabel;
 
 namespace Form_Test
 {
     public class TestButton : Button
     {
         private Color _onColor = Color.LightBlue;
- 
         private Color _offColor = Color.DarkBlue;
-
         private bool _enable;
-
         private Form1 _form1;
-
         private int _x;
-
         private int _y;
+
+        public bool IsEnabled()
+        {
+            return _enable;
+        }
 
         public void SetEnable(bool on)
         {
             _enable = on;
-            if (on)
-            {
-                BackColor = _onColor;
-            }
-            else
-            {
-                BackColor = _offColor;
-            }
+            BackColor = on ? _onColor : _offColor;
         }
 
-        public void Togle() 
+        public void Togle()
         {
             SetEnable(!_enable);
         }
 
-        public TestButton(Form1 form1,int x,int y,Size size, string text)
+        public TestButton(Form1 form1, int x, int y, Size size, string text)
         {
-            //横位置を保管
             _x = x;
-            
-            //縦位置を保管
             _y = y;
-
-            //Form1の参照を保管
             _form1 = form1;
-
-            // ボタンの位置を設定
-            Location = new Point(x * size.Width,y * size.Height);
-
-            // ボタンの大きさを設定
+            Location = new Point(x * size.Width, y * size.Height);
             Size = size;
-
-            // ボタン内のテキストを設定
             Text = text;
-
             SetEnable(false);
-
             Click += ClickEvent;
         }
 
         private void ClickEvent(object sender, EventArgs e)
         {
-            _form1.GetTestButton(_x,_y)?.Togle();
-            _form1.GetTestButton(_x+1,_y)?.Togle();
-            _form1.GetTestButton(_x-1,_y)?.Togle();
-            _form1.GetTestButton(_x,_y+1)?.Togle();
-            _form1.GetTestButton(_x,_y-1)?.Togle();
+            _form1.GetTestButton(_x, _y)?.Togle();
+            _form1.GetTestButton(_x + 1, _y)?.Togle();
+            _form1.GetTestButton(_x - 1, _y)?.Togle();
+            _form1.GetTestButton(_x, _y + 1)?.Togle();
+            _form1.GetTestButton(_x, _y - 1)?.Togle();
+
+            _form1.CheckClear();
         }
     }
 }
-
